78b8624f65a7f739b9cbdd09e5ee1a47ac3c31f8
Neander de Souza <neander.faria@hotmail.com>
2023-10-11 10:41:10 -0300

    getters & setters

--- todolist/src/main/java/br/com/neanderdev/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/neanderdev/todolist/user/UserController.java
@@ -30,6 +30,6 @@ public class UserController {
      */
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+        System.out.println(userModel.getUsername());
     }
 }
--- todolist/src/main/java/br/com/neanderdev/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/neanderdev/todolist/user/UserModel.java
@@ -2,7 +2,32 @@ package br.com.neanderdev.todolist.user;
 
 public class UserModel {
 
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    // getters & setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
