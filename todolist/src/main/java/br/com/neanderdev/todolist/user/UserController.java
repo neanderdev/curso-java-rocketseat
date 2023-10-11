9ab1af5c29add2289c3ba7d99ea09084d6d45062
Neander de Souza <neander.faria@hotmail.com>
2023-10-11 11:12:18 -0300

    usando repository

--- todolist/src/main/java/br/com/neanderdev/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/neanderdev/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.neanderdev.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     /**
      * String (texto)
      * Integer (int) números inteiros
@@ -29,7 +33,9 @@ public class UserController {
      * Body
      */
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+
+        return userCreated;
     }
 }
