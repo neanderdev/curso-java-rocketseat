b82c4f8ec0d535757a8c3a37541e28eb2842dfb8
Neander de Souza <neander.faria@hotmail.com>
2023-10-13 14:11:59 -0300

    update tarefa

--- todolist/src/main/java/br/com/neanderdev/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/neanderdev/todolist/task/TaskController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +52,16 @@ public class TaskController {
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
 
         return tasks;
+    }
+
+    // http://localhost:8080/tasks/bd3841be-4422-4791-b92f-48588b61c0ea
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var idUser = request.getAttribute("idUser");
+
+        taskModel.setIdUser((UUID) idUser);
+        taskModel.setId(id);
+
+        return this.taskRepository.save(taskModel);
     }
 }
